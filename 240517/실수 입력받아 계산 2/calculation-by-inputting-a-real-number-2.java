2dc3818d53076989f398bfaaa0b4dfd11e6bd9d0
heeejaee <162393346+heeejaee@users.noreply.github.com>
2024-05-17 15:28:32 +0900

    Title: 실수 입력받아 계산 2, Time: 148ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240517/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner hee = new Scanner(System.in);
+
+        double a = hee.nextDouble();
+
+        System.out.printf("%.2f" , (a+1.5));
+    }
+}
