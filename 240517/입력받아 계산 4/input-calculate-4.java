9f0f8d84bf01925bc465c5abf75ab6336cf54e99
heeejaee <162393346+heeejaee@users.noreply.github.com>
2024-05-17 15:23:03 +0900

    Title: 입력받아 계산 4, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240517/입력받아 계산 4/input-calculate-4.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner hee = new Scanner(System.in);
+
+        int a = hee.nextInt();
+
+        System.out.println(a*2);
+    }
+}
