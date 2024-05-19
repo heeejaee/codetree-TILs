f536a66b678878e79e3e488e19097cc7d8f8adcb
heeejaee <162393346+heeejaee@users.noreply.github.com>
2024-05-19 21:39:00 +0900

    Title: 날짜 변경하여 출력 2, Time: 169ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240519/날짜 변경하여 출력 2/print-date-with-different-format-2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner hee = new Scanner(System.in);
+
+        hee.useDelimiter("-");
+
+        int a = hee.nextInt();
+        int b = hee.nextInt();
+        int c = hee.nextInt();
+
+        System.out.printf("%d.%d.%d",c,a,b);
+    }
+}
