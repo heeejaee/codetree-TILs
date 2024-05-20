b555cd33b757d26b93ad18a447dc40f9da6fba43
heeejaee <162393346+heeejaee@users.noreply.github.com>
2024-05-20 13:33:23 +0900

    Title: 합과 평균, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240520/합과 평균/sum-and-avg.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner hee = new Scanner(System.in);
+
+        int a = hee.nextInt();
+        int b = hee.nextInt();
+
+        System.out.println((a+b) + " " + (double)(a+b)/2);
+    }
+}
