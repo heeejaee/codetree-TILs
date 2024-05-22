30b2af4fc93ea62293c091db4a84fbba1dd2e1f2
heeejaee <162393346+heeejaee@users.noreply.github.com>
2024-05-22 16:55:20 +0900

    Title: 알파벳에 따른 평가, Time: 146ms, Memory: 10MB, Status: Passed - CodeTree

--- 240522/알파벳에 따른 평가/evaluation-by-alphabet.java
+++ 240522/알파벳에 따른 평가/evaluation-by-alphabet.java
@@ -8,7 +8,7 @@ public class Main {
         String s = hee.nextLine();
 
         if (s.equals("S")) System.out.println("Superior");
-        else if (s.equals("A")) System.out.println("Exellent");
+        else if (s.equals("A")) System.out.println("Excellent");
         else if (s.equals("B")) System.out.println("Good");
         else if (s.equals("C")) System.out.println("Usually");
         else if (s.equals("D")) System.out.println("Effort");
