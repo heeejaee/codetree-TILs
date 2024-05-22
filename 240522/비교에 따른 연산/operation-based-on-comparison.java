2107b91d41897c75905c3b4b5a2f26eecb758c2b
heeejaee <162393346+heeejaee@users.noreply.github.com>
2024-05-22 16:06:42 +0900

    Title: 비교에 따른 연산, Time: 143ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240522/비교에 따른 연산/operation-based-on-comparison.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner hee = new Scanner(System.in);
+
+        int a = hee.nextInt();
+        int b = hee.nextInt();
+
+        if (a > b) {
+            System.out.println(a*b);
+        } else {
+            System.out.println(a/b);
+        }
+    }
+}
