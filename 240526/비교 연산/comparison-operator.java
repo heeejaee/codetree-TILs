93e1541a1b7abf63630147437056c816b9f47c6e
heeejaee <162393346+heeejaee@users.noreply.github.com>
2024-05-26 14:20:12 +0900

    Title: 비교 연산, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240526/비교 연산/comparison-operator.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner hee = new Scanner(System.in);
+
+        int a = hee.nextInt();
+        int b = hee.nextInt();
+
+        if (a >= b) System.out.println("1");
+        else System.out.println("0");
+        
+        if (a > b) System.out.println("1");
+        else System.out.println("0");
+
+        if (a <= b) System.out.println("1");
+        else System.out.println("0");
+
+        if (a < b) System.out.println("1");
+        else System.out.println("0");
+
+        if (a == b) System.out.println("1");
+        else System.out.println("0");
+
+        if (a != b) System.out.println("1");
+        else System.out.println("0");
+    }
+}
